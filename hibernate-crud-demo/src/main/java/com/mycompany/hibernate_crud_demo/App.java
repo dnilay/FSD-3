f2ccd99626bd1bfcd72755d6b8b582902be060be
nilay <nilay@nilay-HP-EliteBook-840-G2>
2019-10-15 12:24:42 +0530

    added dto

--- hibernate-crud-demo/src/main/java/com/mycompany/hibernate_crud_demo/App.java
+++ hibernate-crud-demo/src/main/java/com/mycompany/hibernate_crud_demo/App.java
@@ -1,10 +1,15 @@
 package com.mycompany.hibernate_crud_demo;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.mycompany.hibernate_crud_demo.dao.EmployeeDAO;
+import com.mycompany.hibernate_crud_demo.dao.EmployeeDAOImpl;
 import com.mycompany.hibernate_crud_demo.model.Employee;
+import com.mycompany.hibernate_crud_demo.shared.SharedEmployee;
 
 /**
  * Hello world!
@@ -14,21 +19,19 @@ public class App
 {
     public static void main( String[] args )
     {
+    	EmployeeDAO dao=new EmployeeDAOImpl();
         try {
-        	SessionFactory factory=
-        			new Configuration().configure().
-        			addAnnotatedClass(Employee.class).
-        			buildSessionFactory();
-        	Session session=
-        			factory.getCurrentSession();
-        	Employee employee=new Employee();
-        	employee.setFirstName("John");
-        	employee.setLastName("Doe");
-        	employee.setEmail("john@email.com");
-        	session.getTransaction().begin();
-        	session.persist(employee);
-        	session.getTransaction().commit();
-        	System.out.println("sucess!");
+			/*
+			 * dao.createEmployee(new SharedEmployee("Shane", "Warne",
+			 * "shane@luv2code.com")); System.out.println("inserted");
+			 */
+        	
+        	List<SharedEmployee> se=dao.getAllEmployee();
+        	for(SharedEmployee see:se)
+        	{
+        		System.out.println(see);
+        	}
+        	
 			
 		} catch (Exception e) {
 			// TODO: handle exception
--- hibernate-crud-demo/src/main/java/com/mycompany/hibernate_crud_demo/model/Employee.java
+++ hibernate-crud-demo/src/main/java/com/mycompany/hibernate_crud_demo/model/Employee.java
@@ -13,17 +13,16 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "employee_data")
+@Table(name = "employee")
 public class Employee implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -266053600262638923L;
 	@Id
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(name = "generated_id")
+	private String generatedId;
 	@Column(name = "first_name")
 	private String firstName;
 	@Column(name = "last_name")
