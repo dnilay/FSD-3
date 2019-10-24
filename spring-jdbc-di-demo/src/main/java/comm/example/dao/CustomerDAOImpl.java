401ecafbc13a2360b9e4caa0d78ded650c2ebd97
nilay <nilay@nilay-HP-EliteBook-840-G2>
2019-10-24 16:48:17 +0530

    added @autocommit

--- spring-jdbc-di-demo/src/main/java/comm/example/dao/CustomerDAOImpl.java
+++ spring-jdbc-di-demo/src/main/java/comm/example/dao/CustomerDAOImpl.java
@@ -2,22 +2,23 @@ package comm.example.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import comm.example.model.Customer;
-import lombok.AllArgsConstructor;
 
-
-@AllArgsConstructor
+@Component("customerDao")
 public class CustomerDAOImpl implements CustomerDAO {
 
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public void createCustomer(Customer customer) {
-		jdbcTemplate.update("insert into customer(first_name,last_name,email) values (?,?,?)",
-				customer.getFirstName(),customer.getLastName(),customer.getEmail());
-		
+		jdbcTemplate.update("insert into customer(first_name,last_name,email) values (?,?,?)", customer.getFirstName(),
+				customer.getLastName(), customer.getEmail());
+
 	}
 
 	@Override
@@ -25,6 +26,5 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// TODO Auto-generated method stub
 		return jdbcTemplate.query("select first_name,last_name,email from customer", new CustomerMApper());
 	}
-
 
 }
