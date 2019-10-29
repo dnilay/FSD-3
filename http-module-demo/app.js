d99dc293ede12f3bf8b37ac0634217eaa93d47da
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2019-10-29 12:31:42 +0530

    added put

--- http-module-demo/app.js
+++ http-module-demo/app.js
@@ -1,3 +1,4 @@
+const Joi=require('joi');
 const morgan=require('morgan');
 const express = require('express');
 const app = express();
@@ -43,7 +44,54 @@ app.get("/api/courses/:id",(req,res)=>{
     res.send(course);
 });
 
-let port=3000;
+    
+app.post("/api/courses",(req,res)=>{
+   
+const { error} =validateCourse(req.body);   
+if(error)
+{
+    res.status(400).send(error.details[0].message);
+    return;
+} 
+
+const course={
+    id:courses.length +1,
+    name: req.body.name
+};
+courses.push(course);
+res.send(course);
+
+});
+
+app.put("/api/courses/:id",(req,res)=>{
+    const course=courses.find(c=>c.id==parseInt(req.params.id));
+    
+    if(!course)
+    {
+       res.status(404).send('the course with the given id was not found');
+       return;
+    }
+    const { error} =validateCourse(req.body);   
+    if(error)
+    {
+        res.status(400).send(error.details[0].message);
+        return;
+    } 
+    
+    course.name=req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course)
+{
+    const schema={
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(course,schema);
+}
+
+
+let port=process.env.PORT||3000
 app.listen(port,()=>{
     console.log("server started and listining on port",port);
 })
