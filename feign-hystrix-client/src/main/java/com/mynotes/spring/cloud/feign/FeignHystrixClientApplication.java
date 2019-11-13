53eaead0cd113d909a72930908c7ca508252695e
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2019-11-13 09:19:49 +0530

    initial commit

--- feign-hystrix-client/src/main/java/com/mynotes/spring/cloud/feign/FeignHystrixClientApplication.java
+++ feign-hystrix-client/src/main/java/com/mynotes/spring/cloud/feign/FeignHystrixClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
@@ -11,6 +12,8 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @org.springframework.cloud.openfeign.EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+
+
 public class FeignHystrixClientApplication {
 
 	public static void main(String[] args) {
