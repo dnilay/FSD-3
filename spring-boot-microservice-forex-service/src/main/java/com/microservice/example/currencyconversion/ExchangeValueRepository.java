57b5273bb646e8562d9dc8eaeb1ecdf701b73843
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2019-11-14 12:57:07 +0530

    zipkin/ribbon client

---
+++ spring-boot-microservice-forex-service/src/main/java/com/microservice/example/currencyconversion/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.microservice.example.currencyconversion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends 
+    JpaRepository<ExchangeValue, Long>{
+  ExchangeValue findByFromAndTo(String from, String to);
+}
