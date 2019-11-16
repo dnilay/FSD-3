677bd30cb9c4d12c6be16cc731bdba51d67bd1d5
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2019-11-16 16:07:06 +0530

    init commit

---
+++ spring-aop-mvc-logging/src/main/java/com/example/demo/service/CustomerServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.CustomerDAO;
+import com.example.demo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+	@Transactional
+	public void saveCustomer(Customer theCustomer) {
+
+		customerDAO.saveCustomer(theCustomer);
+	}
+
+	@Transactional
+	public Customer getCustomer(int theId) {
+		
+		return customerDAO.getCustomer(theId);
+	}
+
+	@Transactional
+	public void deleteCustomer(int theId) {
+		
+		customerDAO.deleteCustomer(theId);
+	}
+}
+
+
+
+
+
