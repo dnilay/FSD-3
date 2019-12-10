cb7e625d414fe11a6467dc9b52f4b5ab29d4e914
mailnilaydas@gmail.com <mailnilaydas@gmail.com>
2019-12-10 15:03:08 +0530

    initial test

--- unit-testing/src/main/java/com/unittesting/unittesting/UnitTestingApplication.java
+++ unit-testing/src/main/java/com/unittesting/unittesting/UnitTestingApplication.java
@@ -3,7 +3,7 @@ package com.unittesting.unittesting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   
 public class UnitTestingApplication {
 
 	public static void main(String[] args) {
